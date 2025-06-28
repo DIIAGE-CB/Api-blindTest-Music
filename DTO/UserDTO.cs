75991d001cad0b4209b681611c3dfb0f73a7d056
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-28 22:00:25 +0200

    refactor: remove user

--- DTO/UserDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DTO;
-
-public class UserDTO
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-}
--- DTO/UserGet.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DTO
-{
-    public class UserGet
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
